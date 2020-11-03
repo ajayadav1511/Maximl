e7137c00e6425838da38bd23cf9a56b545a59bf6
ajayadav1511 <ajayadav1511@gmail.com>
2020-11-03 11:19:45 +0530

    Done

--- ex.py
+++ ex.py
@@ -1,25 +1,49 @@
-def longestSubseq(arr, n):
+ 
+NO_OF_CHARS = 256
 
-	# Stores the distinct
-	# array elements
-	s = set()
 
-	# Traverse the input array
-	for i in range(n):
+def max_distinct_char(str, n): 
+
 	
-		# If current element has not
-		# occurred previously
-		if (arr[i] not in s):
+	count = [0] * NO_OF_CHARS 
+	
 
-			# Insert it into set
-			s.add(arr[i])
+	for i in range(n): 
+		count[ord(str[i])] += 1
+	
+	max_distinct = 0
+	for i in range(NO_OF_CHARS): 
+		if (count[i] != 0): 
+			max_distinct += 1	
+	
+	return max_distinct 
 
-	return len(s)
+def smallesteSubstr_maxDistictChar(str): 
 
-# Given array
-arr = "abcda"
-n = len(arr)
+	n = len(str)
+	max_distinct = max_distinct_char(str, n) 
+	minl = n	 
+	
+	
+	for i in range(n): 
+		for j in range(n): 
+			subs = str[i:j] 
+			subs_lenght = len(subs) 
+			sub_distinct_char = max_distinct_char(subs,subs_lenght) 
+			
+			
+			if (subs_lenght < minl and
+				max_distinct == sub_distinct_char): 
+				minl = subs_lenght 
 
-# Function Call
-print(longestSubseq(arr, n))
+	return minl 
+
+
+if __name__ == "__main__": 
+	
+	# Input String 
+	str = "AABBBCBB"
+	
+	l = smallesteSubstr_maxDistictChar(str) 
+	print( "The length of the smallest substring", "consisting of maximum distinct","characters =", l) 
 
